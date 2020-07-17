155474d8a2856c73f4cba87144e7f56e17e86f61
Somnath Paul <Somnath4x@gmail.com>
2020-07-17 20:46:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let currentTime = new Date().toLocaleTimeString();
+  const [time, setCount] = useState();
+
+  const getTime = () => {
+     currentTime = new Date().toLocaleTimeString();
+     setCount(currentTime);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>{time}</h1>
+      <button class="button" onClick={getTime}>Get time</button>
       </header>
     </div>
   );
