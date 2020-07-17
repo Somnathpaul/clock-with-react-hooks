ade48a0e58002e7e087f6a20c0f59a3adc4dd788
Somnath Paul <Somnath4x@gmail.com>
2020-07-17 21:04:48 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import './App.css';
 function App() {
 
   let currentTime = new Date().toLocaleTimeString();
-  const [time, setCount] = useState();
+  const [time, setCount] = useState(currentTime);
 
   const getTime = () => {
      currentTime = new Date().toLocaleTimeString();
      setCount(currentTime);
   }
 
+  setInterval(getTime, 1000);
+
   return (
     <div className="App">
       <header className="App-header">
       <h1>{time}</h1>
-      <button class="button" onClick={getTime}>Get time</button>
+      <button className="button" onClick={getTime}>Get time</button>
       </header>
     </div>
   );
